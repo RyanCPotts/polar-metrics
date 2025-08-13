9bd1ecb47c8767b6360a631f1ede3f48fd602a5f
RYAN <ryancpotts@gmail.com>
2025-08-12 21:48:22 -0500

    readme for future

--- App.js
+++ App.js
@@ -1,4 +1,3 @@
-// App.js
 import React from 'react';
 import GoogleCivicData from './GoogleCivicData';  // Correct import
 import ProPublicaData from './ProPublicaDataAPI';  // Correct import to ProPublicaDataAPI.js
@@ -8,6 +7,7 @@ const App = () => {
     <div>
       <h1>Google Civic Information</h1>
       <GoogleCivicData address="1600 Amphitheatre Parkway, Mountain View, CA 94043" />  {/* Correct usage of component */}
+      
       <h1>ProPublica Data</h1>
       <ProPublicaData />  {/* Correct usage without address prop */}
     </div>
--- README.txt
+++
@@ -1,5 +0,0 @@
-Working on implementing APIs for mass data collection.
-
-Politics.
-
-> I'm interested in developing a political tracking app which hybrids traditional geo targeting with API grabs like CongressGovAPI.js, ProPublicaDataAPI.js, GoogleCivicDataAPI.js
